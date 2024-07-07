dd11f0e234f3634302edd17afeef3a408171dd0b
Dan Habot <dan.habot@gmail.com>
2024-07-07 02:16:34 -0400

    created diff code

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,3 +1,4 @@
+use diff::Result as DiffResult;
 use std::path::{Path, PathBuf};
 
 pub const REPO_METADATA_DIR_NAME: &str = ".duh";
@@ -34,4 +35,74 @@ pub fn get_cwd() -> String {
         .to_str()
         .unwrap_or_default()
         .to_owned()
+}
+
+#[derive(PartialEq, Eq, Clone)]
+pub enum DiffFragment {
+    ADDED { offset: u64, body: Vec<u8> },
+    UNCHANGED { offset: u64, len: u64 },
+    DELETED { offset: u64, len: u64 },
+}
+
+pub fn diff_content(old: &[u8], new: &[u8]) -> Vec<DiffFragment> {
+    let delta = diff::slice(old, new);
+
+    // let mut frags = Vec::<DiffFragment>::new();
+    let mut squished_frags = Vec::<DiffFragment>::new();
+
+    for (i, d) in delta.iter().enumerate() {
+        if squished_frags.len() <= 0 {
+            match d {
+                DiffResult::Left(_) => squished_frags.push(DiffFragment::DELETED {
+                    offset: i as u64,
+                    len: 1,
+                }),
+                DiffResult::Both(_, _) => squished_frags.push(DiffFragment::UNCHANGED {
+                    offset: i as u64,
+                    len: 1,
+                }),
+                DiffResult::Right(b) => squished_frags.push(DiffFragment::ADDED {
+                    offset: i as u64,
+                    body: vec![**b],
+                }),
+            }
+            
+            continue;
+        }
+
+        let l = squished_frags.last_mut().unwrap();
+
+        match (l, d) {
+            (
+                DiffFragment::ADDED {
+                    offset: _,
+                    body: last_body,
+                },
+                DiffResult::Left(d),
+            ) => {
+                last_body.push(**d);
+            }
+            (
+                DiffFragment::UNCHANGED {
+                    offset: _,
+                    len: last_len,
+                },
+                DiffResult::Both(_, _),
+            ) => {
+                *last_len += 1;
+            }
+            (
+                DiffFragment::DELETED {
+                    offset: _,
+                    len: last_len,
+                },
+                DiffResult::Right(_),
+            ) => {
+                *last_len += 1;
+            }
+            _ => panic!("how??"),
+        }
+    }
+
+    squished_frags
 }
